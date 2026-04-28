13cdab4448bbb6e7bf1964462c00552f44d66e44
bauntyrosh67-ship-it <bauntyrosh67@gmail.com>
2026-04-28 15:41:53 +0600

    Add files via upload

---
+++ gameVS.cpp
@@ -0,0 +1,7 @@
+<Solution>
+  <Configurations>
+    <Platform Name="x64" />
+    <Platform Name="x86" />
+  </Configurations>
+  <Project Path="roblox2/roblox2.vcxproj" Id="b1c00481-498a-4451-93c0-5557d0431913" />
+</Solution>
